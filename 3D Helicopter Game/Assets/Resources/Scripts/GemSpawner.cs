9d2b42c52a3ad6fa9174c521c0f77a72acc6e5fe
Ader Einstein <adereinstein1>
2019-09-05 15:34:20 -0700

    initial commit

---
+++ 3D Helicopter Game/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnGems() {
+
+        while (true)
+        {
+            // number of gems we could spawn vertically
+            int gemsThisRow = Random.Range(1, 3);
+
+            // instantiate all gems in this row separated by some random amount of space
+            for (int i = 0; i < gemsThisRow; i++)
+            {
+                Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+            }
+            
+            // Pause 5-15 seconds until the next Gem spawns
+            yield return new WaitForSeconds(Random.Range(5, 15));
+        }
+        
+    }
+}
+
+
+
